0a8d166e0fc5b7bd3c24261dd776dc1193877e13
vaibhavgaikwad8476 <rajnandinitikkal@gmail.com>
2023-11-25 10:37:42 +0530

    add excelr assignments

---
+++ JavaScript Demo/script.js
@@ -0,0 +1,28 @@
+valueNum=3;
+valueFloat=2.3;
+valueNan="Hello"/2;
+console.log(valueNum);
+console.log(valueFloat);
+console.log(valueNan);
+
+myname="Rutuja";
+console.log(myname);
+lastname='Tikkal';
+console.log(lastname);
+
+a=null;
+console.log(a);
+let b;
+console.log(b);
+
+one=a;
+two=b;
+console.log(one==two);
+
+var value;
+console.log(value);
+value=24;
+
+let val;
+console.log(val);
+val=45;
