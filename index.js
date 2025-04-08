62465776fe3dc65dae835a01673e3a81283056b0
Rajnandini_Tikkal <109601073+rajnandinitikkal@users.noreply.github.com>
2025-04-08 14:43:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+// prompt("What is your age ?")
+
+// console.log("Hii there !!");
+
+// let a = 10;
+// let b = 20;
+// let c = a + b;
+// console.log(c);
+
+// console.log(typeof[1,2,3]);
+// console.log("Hello")
+
+//  Strings
+
+// let brand = "Rutuja";
+// let founder =" myMan";
+
+// let x = "The " + brand + " is founded by " + founder;
+
+// document.write(x);
+// console.log(x[1]);
+
+// let text = "Tushar, Rutuja, Mohini, Nanasaheb";
+
+// let y = text.concat(" is a good boy");
+// let y = text.lastIndexOf("a")
+// let y = text.replace('a','y');
+// let y = text.toUpperCase();
+// let y = text.toLowerCase();
+// let y = text.split("y");
+
+// text = 'studyHard';
+
+// let y = text.slice(0,9)
+// let y = text.substring(5)
+
+// console.log(y);
